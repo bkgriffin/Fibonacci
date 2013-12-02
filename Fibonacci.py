663e4b883fd1cab576c68effeec6706ad0988431
kitsune-san <brandon@Brandons-MacBook-Pro.local>
2013-12-01 22:35:32 -0500

    - Added "TODO:" section

--- Fibonacci.py
+++ Fibonacci.py
@@ -19,6 +19,12 @@
 #
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million,
 # find the sum of the even-valued terms.
+#
+#
+#
+# TODO:
+# - Add ability for user to enter an upper-bound for the Fibonacci sequence summation.
+
 
 class Fibonacci:
     """
